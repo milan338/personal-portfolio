b3715e8a52b4ec121b3251908bef2584a3c2b5e2
Milan <milan.338@outlook.com>
2022-12-27 22:50:53 +1030

    Add custom useWindowSize hook

---
+++ src/hooks/window.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+type WindowSize = [width: number, height: number];
+
+/**
+ * Custom React hook to get the current window size.
+ * Will cause a component rerender whenever the window size changes.
+ * @returns The current window dimensions
+ */
+export function useWindowSize() {
+    const [windowSize, setWindowSize] = useState<WindowSize>([-1, -1]);
+
+    useEffect(() => {
+        // Can't observe the document.body element since it will give the wrong height
+        const windowSizeElement = document.querySelector('#window-size');
+        if (windowSizeElement === null) throw new Error('Failed to get window size');
+
+        // Resize observer correctly identifies resizes where window.addEventListener('resize', cb) fails to
+        const observer = new ResizeObserver((entries) => {
+            const [entry] = entries;
+            const { width, height } = entry.contentRect;
+            setWindowSize([width, height]);
+        });
+
+        observer.observe(windowSizeElement);
+
+        return () => observer.unobserve(windowSizeElement);
+    }, []);
+
+    // Avoid SSR errors
+    if (typeof window === 'undefined') return [0, 0];
+
+    // Cause single reflow on component mount to get initial width / height
+    if (windowSize[0] === -1 && windowSize[1] === -1)
+        setWindowSize([
+            document.documentElement.clientWidth,
+            document.documentElement.clientHeight,
+        ]);
+
+    return windowSize;
+}
