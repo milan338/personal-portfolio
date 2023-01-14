ff5b5802d3bb65104e39e6f31bbbb07e55deb7be
Milan <milan.338@outlook.com>
2023-01-14 22:37:54 +1030

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const fonts = require('./src/styles/fonts.json');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        huge: '10rem',
+      },
+      fontFamily: {
+        sans: [`var(${fonts['--font-poppins']})`, ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
