bd3006b232c194cd2ddd024edd612155f61e0539
Milan <milan.338@outlook.com>
2023-02-01 11:46:47 +1030

    Use tailwind animations library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const fonts = require('./src/styles/fonts.json');
 
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('tailwindcss').Config}
+ */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      transitionDuration: {
+        3000: '3000ms',
+      },
       fontFamily: {
         sans: [`var(${fonts['--font-poppins']})`, ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
