cb069a0c52dc4ff950b9687a31e2f85a1db53715
Milan <milan.338@outlook.com>
2023-02-01 21:31:20 +1030

    Adjust links styles

--- src/components/content/MainContent.tsx
+++ src/components/content/MainContent.tsx
@@ -19,22 +19,26 @@ const SECTIONS = {
 export default function MainContent(props: MainContentProps) {
     const { children } = props;
     const path = usePathname();
+    const sections = Object.entries(SECTIONS);
 
-    const links = Object.entries(SECTIONS).map(([href, heading]) => {
+    const links = sections.map(([href, heading], i) => {
         const active = href === path || (path === '/' && href === '/projects');
 
         return (
-            <Link
-                key={href}
-                className={`text-4xl font-medium motion-safe:transition-opacity ${
-                    active ? 'opacity-100' : 'opacity-[0.45]'
-                }`}
-                href={href}
-                aria-checked={active}
-                aria-expanded={active}
-            >
-                {heading}
-            </Link>
+            <>
+                <Link
+                    key={href}
+                    className={`text-4xl font-medium motion-safe:transition-opacity ${
+                        active ? 'opacity-100' : 'opacity-[0.45] hover:opacity-60'
+                    }`}
+                    href={href}
+                    aria-checked={active}
+                    aria-expanded={active}
+                >
+                    {heading}
+                </Link>
+                {i < sections.length - 1 && <div className="h-full w-1 bg-black opacity-10" />}
+            </>
         );
     });
 
@@ -44,7 +48,7 @@ export default function MainContent(props: MainContentProps) {
             className="relative z-10 flex h-auto w-[min(80vw,_48rem)] flex-col items-center"
         >
             <HeroSection />
-            <div className="flex w-full flex-col justify-start gap-9 pt-12 sm:flex-row">
+            <div className="flex w-full flex-col justify-start gap-8 pt-12 sm:flex-row">
                 {links}
             </div>
             {children}
