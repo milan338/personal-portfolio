30c45a66fb8b7b22c4108fc3e6a99be73d4cb46f
Milan <milan.338@outlook.com>
2023-06-12 12:17:21 +0930

    Update analytics component

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -5,9 +5,7 @@ const ANALYTICS_SRC = DEV
     ? 'https://cdn.vercel-insights.com/v1/script.debug.js'
     : '/_vercel/insights/script.js';
 
-const ANALYTICS_INTEGRITY = DEV
-    ? 'sha512-+ELKecaEcLta4OSaZdoj6J1RPTyDI1LgtDnt/Yq5nMbDtOdE+TieuKOL5Mb7NZ465WUCwyut9DO3DgeOisgqDQ=='
-    : 'sha512-sXFK4flWBTfuIKZUf9WqzwL9ot/NFoQnLQEwvAJbdDe4I9ICNpzZztdBo1clxh8jRjtWtZrbpiuDJqR2+5EBKQ==';
+const ANALYTICS_INTEGRITY = DEV ? undefined : process.env.ANALYTICS_INTEGRITY;
 
 export default function Analytics() {
     return (
